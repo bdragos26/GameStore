15a76baff22a51f41f2f916c1883f1cfbf745f39
bdragos26 <dragosburtea@gmail.com>
2025-07-23 11:41:13 +0300

    added pagination and card for games

--- GameStore.Shared/Models/Game.cs
+++ GameStore.Shared/Models/Game.cs
@@ -13,5 +13,6 @@ namespace GameStore.Shared.Models
         [Range(1, 100)]
         public double Price { get; set; }
         public DateOnly ReleaseDate { get; set; }
+        public string ImageUrl { get; set; }
     }
 }
