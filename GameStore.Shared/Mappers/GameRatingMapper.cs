4efa0be4bb8cb85cb5f497e67da9fb502143ee93
bdragos26 <dragosburtea@gmail.com>
2025-08-05 13:21:38 +0300

    game rating mapper

---
+++ GameStore.Shared/Mappers/GameRatingMapper.cs
@@ -0,0 +1,18 @@
+﻿using GameStore.Shared.DTOs;
+using GameStore.Shared.Models;
+
+namespace GameStore.Shared.Mappers
+{
+    public static class GameRatingMapper
+    {
+        public static GameRatingDTO ToDto(IGrouping<Game?, GameRating> group)
+        {
+            return new GameRatingDTO
+            {
+                Game = group.Key,
+                AverageScore = group.Average(r => r.Score),
+                RatingCount = group.Count()
+            };
+        }
+    }
+}
