a8762ea0f76162836a87020248d237200791a340
bdragos26 <dragosburtea@gmail.com>
2025-08-05 11:10:33 +0300

    cart and client tests

---
+++ GameStore.E2ETests/CartTests.cs
@@ -0,0 +1,45 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GameStore.E2ETests
+{
+    public class CartTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public CartTests()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            _driver = new ChromeDriver(options);
+        }
+
+        [Fact]
+        public void AddGameToCart_ShouldShowConfirmationMessage()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:7194");
+            Thread.Sleep(2000);
+
+            var catalogNavLink = _driver.FindElement(By.XPath("//a[contains(text(), 'Game Catalog')]"));
+            catalogNavLink.Click();
+            Thread.Sleep(1000);
+
+            var firstGameCard = _driver.FindElement(By.XPath("(//div[contains(@class, 'card')])[1]"));
+            firstGameCard.Click();
+            Thread.Sleep(1000);
+
+            var addToCartButton = _driver.FindElement(By.XPath("//button[contains(text(), 'Add to Cart')]"));
+            addToCartButton.Click();
+            Thread.Sleep(1000);
+
+            var confirmation = _driver.FindElement(By.XPath("//*[contains(text(), 'Added to cart!')]"));
+            Assert.NotNull(confirmation);
+        }
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
---
+++ GameStore.E2ETests/ClientCredentialsTests.cs
@@ -0,0 +1,112 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace GameStore.E2ETests
+{
+    public class ClientCredentialsTests : IDisposable
+    {
+        private readonly IWebDriver _driver;
+
+        public ClientCredentialsTests()
+        {
+            var options = new ChromeOptions();
+            options.AddArgument("--start-maximized");
+            _driver = new ChromeDriver(options);
+        }
+
+        [Fact]
+        public void RegisterPage_ShouldLoadProperly()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:7194");
+            Thread.Sleep(2000);
+
+            var registerNavLink = _driver.FindElement(By.XPath("//a[contains(text(), 'Register')]"));
+            registerNavLink.Click();
+            Thread.Sleep(2000);
+
+            var title = _driver.FindElement(By.XPath("//h3[text()='Register new account']"));
+            Assert.NotNull(title);
+
+            var usernameInput = _driver.FindElement(By.XPath("//input[@id='username']"));
+            usernameInput.SendKeys("TestUser123");
+
+            var emailInput = _driver.FindElement(By.XPath("//input[@id='email']"));
+            emailInput.SendKeys("test@example.com");
+
+            var passwordInput = _driver.FindElement(By.XPath("//input[@id='password']"));
+            passwordInput.SendKeys("SecurePassword123!");
+
+            var firstNameInput = _driver.FindElement(By.XPath("//input[@id='firstName']"));
+            firstNameInput.SendKeys("Test");
+
+            var lastNameInput = _driver.FindElement(By.XPath("//input[@id='lastName']"));
+            lastNameInput.SendKeys("User");
+
+            var registerButton = _driver.FindElement(By.XPath("//button[@type='submit']"));
+            registerButton.Click();
+
+            Thread.Sleep(3000);
+
+            Assert.Contains("localhost", _driver.Url);
+        }
+
+        [Fact]
+        public void Login_ShouldSucceed_WithValidCredentials()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:7194");
+            Thread.Sleep(2000);
+
+            var loginNavLink = _driver.FindElement(By.XPath("//a[contains(text(), 'Login')]"));
+            loginNavLink.Click();
+            Thread.Sleep(2000);
+
+            var usernameInput = _driver.FindElement(By.XPath("//input[@id='username']"));
+            usernameInput.SendKeys("TestUser123");
+
+            var passwordInput = _driver.FindElement(By.XPath("//input[@id='password']"));
+            passwordInput.SendKeys("SecurePassword123!");
+
+            var loginButton = _driver.FindElement(By.XPath("//button[@type='submit']"));
+            loginButton.Click();
+
+            Thread.Sleep(1000);
+
+            var errorMessages = _driver.FindElements(By.XPath("//*[contains(text(), 'Invalid username or password')]"));
+            Assert.Empty(errorMessages);
+
+            Assert.Contains("localhost", _driver.Url);
+        }
+
+        [Fact]
+        public void Login_ShouldFail_WithInvalidCredentials()
+        {
+            _driver.Navigate().GoToUrl("https://localhost:7194");
+            Thread.Sleep(2000);
+
+            var loginNavLink = _driver.FindElement(By.XPath("//a[contains(text(), 'Login')]"));
+            loginNavLink.Click();
+            Thread.Sleep(2000);
+
+            var usernameInput = _driver.FindElement(By.XPath("//input[@id='username']"));
+            usernameInput.SendKeys("WrongUser");
+
+            var passwordInput = _driver.FindElement(By.XPath("//input[@id='password']"));
+            passwordInput.SendKeys("WrongPassword!");
+
+            var loginButton = _driver.FindElement(By.XPath("//button[@type='submit']"));
+            loginButton.Click();
+
+            Thread.Sleep(1000);
+
+            var errorMessage = _driver.FindElement(By.XPath("//*[contains(text(), 'Invalid username or password')]"));
+            Assert.NotNull(errorMessage);
+        }
+
+
+        public void Dispose()
+        {
+            _driver.Quit();
+            _driver.Dispose();
+        }
+    }
+}
